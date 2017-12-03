1de7d339e0f5f389cfa15b6b044daf9313f6e4c8
omarvch <omar.vasquez.chavarri@gmail.com>
2017-12-03 15:41:36 -0500

    Uso de bootstrap

--- js/action.js
+++ js/action.js
@@ -1,10 +1,1 @@
-function mostarMas(){
-	var masinfo = document.getElementById("textoResumen");
-
-	if(masinfo.style.display == "none"){
-		masinfo.style.display = "inline";	
-	}else{
-		masinfo.style.display = "none"
-	}
-	
 }
