c39bc2597146701beb040cbab585f6590e000f4c
omarvch <omar.vasquez.chavarri@gmail.com>
2017-12-03 12:04:36 -0500

    Clase java script

---
+++ js/action.js
@@ -0,0 +1,10 @@
+function mostarMas(){
+	var masinfo = document.getElementById("textoResumen");
+
+	if(masinfo.style.display == "none"){
+		masinfo.style.display = "inline";	
+	}else{
+		masinfo.style.display = "none"
+	}
+	
+}
